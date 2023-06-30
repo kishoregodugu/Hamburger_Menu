380484db25f8bb89d64f4ac786de6a1d6feb5ae7
kishore <kishoreexample.com>
2023-06-30 10:20:13 +0000

    Hamburger Menu

---
+++ src/components/Home/index.js
@@ -0,0 +1,21 @@
+// Write your code here
+import Header from '../Header'
+import './index.css'
+
+const Home = () => (
+  <div className="home-container">
+    <Header />
+    <img
+      className="mobile-home-image"
+      src="https://assets.ccbp.in/frontend/react-js/home-sm-img.png"
+      alt="home"
+    />
+    <img
+      className="desktop-home-image"
+      src="https://assets.ccbp.in/frontend/react-js/home-lg-img.png"
+      alt="home"
+    />
+  </div>
+)
+
+export default Home
